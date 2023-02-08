97412254b3b9f9ebe0ea1115f007acad907eb874
Vitályos Márton <marci12345678956@gmail.com>
2023-02-08 09:56:33 +0100

    Add DriverManager.java file to initialize webdriver and BasePage.java file with login function

---
+++ src/test/java/base/BasePage.java
@@ -0,0 +1,67 @@
+package base;
+
+import factory.DriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+
+public class BasePage {
+    @FindBy(id = "login-form-username")
+    protected WebElement usernameField;
+
+    @FindBy(id = "login-form-password")
+    protected WebElement passwordField;
+
+    @FindBy(id = "login")
+    protected WebElement loginButton;
+
+    @FindBy(id = "header-details-user-fullname")
+    protected WebElement profilePicture;
+
+    protected WebDriver webdriver;
+    protected WebDriverWait webDriverWait;
+
+
+    public BasePage() {
+        this.webdriver = new DriverManager().initializeDriver();
+        this.webDriverWait = new WebDriverWait(webdriver, Duration.ofSeconds(15));
+        PageFactory.initElements(webdriver, this);
+    }
+
+
+    public void setUSERNAME(String username) {
+        usernameField.sendKeys(username);
+    }
+
+    public void setPASSWORD(String password) {
+        passwordField.sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        loginButton.click();
+    }
+
+    public void waitForProfilePictureIsVisible() {
+        profilePicture = webDriverWait.until(ExpectedConditions.visibilityOf(profilePicture));
+    }
+
+
+    public void basicLoginWithCorrectCredentials() {
+        setUSERNAME("Automation31");
+        setPASSWORD("CCAutoTest19.");
+        clickLoginButton();
+        waitForProfilePictureIsVisible();
+    }
+
+    public void quitDriver() {
+        webdriver.quit();
+    }
+
+
+}
